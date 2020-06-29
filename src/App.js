5d72d88c87371a33505ee87988ebb333f07c2da4
Miller <macorreag@unal.edu.co>
2020-06-28 23:05:48 -0500

    test apollo with rickandmortyapi

--- src/App.js
+++ src/App.js
@@ -4,6 +4,33 @@ import { Navbar, NavbarBrand } from 'reactstrap';
 import Publications from './components/Publications'
 import './App.css';
 
+import ApolloClient , { gql } from "apollo-boost";
+import { ApolloProvider,  useQuery} from "@apollo/react-hooks";
+
+function CharactersQuery() {
+  const { loading, error, data } = useQuery(gql`
+    {
+      characters {
+        results {
+          id
+          name
+        }
+      }
+    }
+  `);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+  return data.characters.results.map(({ id, name }) => (
+    <div key={id}>
+      <p>
+        {id}: {name}
+      </p>
+    </div>
+  ));
+}
+
 function App() {
   return (
     <div className="App">
@@ -14,6 +41,7 @@ function App() {
           </NavbarBrand>
         </div>
       </Navbar>
+      <CharactersQuery />
       <Publications />
     </div>
   );
